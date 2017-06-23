2d877ede6f0d830834c148dde60b73a35a1a5a91
saraheig <sarah.composto@heig-vd.ch>
2017-06-23 16:29:44 +0000

    Add create features table migration

---
+++ database/migrations/2017_06_23_162038_create_features_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeaturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('features', function (Blueprint $table) {
+            // Columns of the features table
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->float('coordX', 10, 8);
+            $table->float('coordY', 10, 8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
