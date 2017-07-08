986509c79a3de219675f0daffa9d515737b88da8
saraheig <sarah.composto@heig-vd.ch>
2017-07-08 12:33:33 +0000

    Add features in migration

--- database/migrations/2017_06_23_162038_create_features_table.php
+++ database/migrations/2017_06_23_162038_create_features_table.php
@@ -32,6 +32,27 @@ class CreateFeaturesTable extends Migration
         $feature3 = ['name' => 'Castle', 'description' => 'A lot of things to see',
             'coordLat' => '46.77845', 'coordLng' => '6.64157'];
         DB::table('features')->insert($feature3);
+        $feature4 = ['name' => 'Sport', 'description' => 'Time to move!',
+            'coordLat' => '46.78016', 'coordLng' => '6.6545'];
+        DB::table('features')->insert($feature4);
+        $feature5 = ['name' => 'Station', 'description' => 'Let\'s travel!',
+            'coordLat' => '46.78163', 'coordLng' => '6.64094'];
+        DB::table('features')->insert($feature5);
+        $feature6 = ['name' => 'St-Roch', 'description' => 'St-Roch center',
+            'coordLat' => '46.78137', 'coordLng' => '6.64742'];
+        DB::table('features')->insert($feature6);
+        $feature7 = ['name' => 'Swimming pool', 'description' => 'A little swim?',
+            'coordLat' => '46.78525', 'coordLng' => '6.64623'];
+        DB::table('features')->insert($feature7);
+        $feature8 = ['name' => 'Skating rink', 'description' => 'One activity for winter',
+            'coordLat' => '46.78413', 'coordLng' => '6.64518'];
+        DB::table('features')->insert($feature8);
+        $feature9 = ['name' => 'Tennis club', 'description' => 'Time to move!',
+            'coordLat' => '46.78433', 'coordLng' => '6.64691'];
+        DB::table('features')->insert($feature9);
+        $feature10 = ['name' => '"Les Bains"', 'description' => 'Let\'s relax!',
+            'coordLat' => '46.77164', 'coordLng' => '6.64809'];
+        DB::table('features')->insert($feature10);
     }
 
     /**
